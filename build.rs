b23c000a189c793c6a5342af05287f92e6b2d416
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 15:37:33 +0100

    Added CRYPTO.

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@ use std::env;
 fn main() {
     pkg_config::probe_library("mysqlclient").unwrap();
     pkg_config::probe_library("libssl").unwrap();
+    pkg_config::probe_library("libcrypto").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
     println!("cargo:rustc-link-lib=static=ssl");
+    println!("cargo:rustc-link-lib=static=crypto");
 }
