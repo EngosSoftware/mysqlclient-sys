902dd1b63af9fcf69572f2b3641be56cc5cf2928
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 15:12:31 +0100

    Added pkg_config

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
+extern crate pkg_config;
+
 use std::env;
 
 fn main() {
-    if let Ok(path) = env::var("MYSQLCLIENT_LIB_DIR") {
+    if pkg_config::probe_library("mysqlclient").is_ok() {
+        // ok
+    } else if let Ok(path) = env::var("MYSQLCLIENT_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", path);
     }
     println!("cargo:rustc-link-lib=static=mysqlclient");
