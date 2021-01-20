feb639ea58a5165ecdd463822ba78267001905b6
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 15:32:40 +0100

    Fixed SSL

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::env;
 
 fn main() {
     pkg_config::probe_library("mysqlclient").unwrap();
-    pkg_config::probe_library("ssl").unwrap();
+    pkg_config::probe_library("libssl").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
     println!("cargo:rustc-link-lib=static=ssl");
 }
