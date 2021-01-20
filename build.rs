fce7076c989d3eaab147da0b2384150866044140
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 17:34:46 +0100

    Try 128.

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
+    println!("-lmysqlclient -lssl -lcrypto -lcrypt");
     println!("cargo:rustc-link-lib=static=mysqlclient");
     println!("cargo:rustc-link-lib=static=ssl");
     println!("cargo:rustc-link-lib=static=crypto");
+    println!("cargo:rustc-link-lib=static=crypt");
 }
