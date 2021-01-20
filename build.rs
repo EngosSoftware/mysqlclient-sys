8a223661b2dba8bab19ff9dc3edce7d040f2ac3c
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 17:50:16 +0100

    Try 131.

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::Config::new().statik(true).probe("mysqlclient").unwrap();
+    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-lib=static=mysqlclient");
     println!("cargo:rustc-link-lib=static=ssl");
     println!("cargo:rustc-link-lib=static=crypto");
