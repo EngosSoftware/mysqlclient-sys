654a6ce7e53715b2f6a283c2b46417529c8576fb
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 16:06:39 +0100

    Fixed probe.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::Config::new().statik(true).probe_library("mysqlclient").unwrap();
+    pkg_config::Config::new().statik(true).probe("mysqlclient").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
 }
