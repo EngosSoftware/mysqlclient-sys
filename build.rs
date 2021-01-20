670935e9b35146ed27e7a1bdddb6e2ab3bc0ef28
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 15:28:48 +0100

    Added SSL dependency.

--- build.rs
+++ build.rs
@@ -3,10 +3,8 @@ extern crate pkg_config;
 use std::env;
 
 fn main() {
-    if pkg_config::probe_library("mysqlclient").is_ok() {
-        // ok
-    } else if let Ok(path) = env::var("MYSQLCLIENT_LIB_DIR") {
-        println!("cargo:rustc-link-search=native={}", path);
-    }
+    pkg_config::probe_library("mysqlclient").unwrap();
+    pkg_config::probe_library("ssl").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
+    println!("cargo:rustc-link-lib=static=ssl");
 }
