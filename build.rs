8339345850d8b75e4eae0c27816000375501e215
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 17:32:30 +0100

    Simplified.

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
-extern crate pkg_config;
-
 fn main() {
-    pkg_config::Config::new().statik(true).probe("mysqlclient").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
-    println!("cargo:rustc-link-lib=static=pthread");
-    println!("cargo:rustc-link-lib=static=rt");
-    println!("cargo:rustc-link-lib=static=dl");
-    println!("cargo:rustc-link-lib=static=resolv");
     println!("cargo:rustc-link-lib=static=ssl");
     println!("cargo:rustc-link-lib=static=crypto");
 }
