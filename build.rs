a549a15a0995f7e8669baeac0754e53a6dff434b
Dariusz Depta <dariusz.depta@engos.software>
2021-01-20 16:04:57 +0100

    Changed to statik.

--- build.rs
+++ build.rs
@@ -1,12 +1,6 @@
 extern crate pkg_config;
 
-use std::env;
-
 fn main() {
-    pkg_config::probe_library("mysqlclient").unwrap();
-    pkg_config::probe_library("libssl").unwrap();
-    pkg_config::probe_library("libcrypto").unwrap();
+    pkg_config::Config::new().statik(true).probe_library("mysqlclient").unwrap();
     println!("cargo:rustc-link-lib=static=mysqlclient");
-    println!("cargo:rustc-link-lib=static=ssl");
-    println!("cargo:rustc-link-lib=static=crypto");
 }
